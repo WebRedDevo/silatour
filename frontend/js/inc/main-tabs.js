cadc4d79b1d8473f0543c40565b5dd98939a63fd
Smirennikov <evgenii.smirennikov@gmail.com>
2018-12-18 09:37:35 +0300

    header flex and mauch aditional main tabs

--- frontend/js/inc/main-tabs.js
+++ frontend/js/inc/main-tabs.js
@@ -1,12 +1,23 @@
-document.querySelectorAll('.aside-list__item')[0].addEventListener('click', function(){
-  document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active')
-  document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active')
-  document.querySelectorAll('.aside-list__item')[0].classList.toggle('active')
-  document.querySelectorAll('.aside-list__item')[1].classList.toggle('active')
-})
-document.querySelectorAll('.aside-list__item')[1].addEventListener('click', function(){
-  document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active')
-  document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active')
-  document.querySelectorAll('.aside-list__item')[0].classList.toggle('active white')
-  document.querySelectorAll('.aside-list__item')[1].classList.toggle('active white')
-})
+let mainTabs = document.querySelectorAll('.aside-list__item')[0];
+
+if (mainTabs !== undefined){
+  document.querySelector('html').classList.remove('night');
+  document.querySelectorAll('.aside-list__item')[0].addEventListener('click', function(){
+    document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active')
+    document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('white')
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('white')
+    document.querySelector('html').classList.remove('night');
+  })
+  document.querySelectorAll('.aside-list__item')[1].addEventListener('click', function(){
+    document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active')
+    document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('active')
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('white')
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('white')
+    document.querySelector('html').classList.add('night');
+  })
+}
--- public/js/lib.js
+++ public/js/lib.js
@@ -160,18 +160,29 @@ if (localStorage.getItem('lightMode') === 'on') {
 }
 "use strict";
 
-document.querySelectorAll('.aside-list__item')[0].addEventListener('click', function () {
-  document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active');
-  document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active');
-  document.querySelectorAll('.aside-list__item')[0].classList.toggle('active');
-  document.querySelectorAll('.aside-list__item')[1].classList.toggle('active');
-});
-document.querySelectorAll('.aside-list__item')[1].addEventListener('click', function () {
-  document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active');
-  document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active');
-  document.querySelectorAll('.aside-list__item')[0].classList.toggle('active white');
-  document.querySelectorAll('.aside-list__item')[1].classList.toggle('active white');
-});
+var mainTabs = document.querySelectorAll('.aside-list__item')[0];
+
+if (mainTabs !== undefined) {
+  document.querySelector('html').classList.remove('night');
+  document.querySelectorAll('.aside-list__item')[0].addEventListener('click', function () {
+    document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active');
+    document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('white');
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('white');
+    document.querySelector('html').classList.remove('night');
+  });
+  document.querySelectorAll('.aside-list__item')[1].addEventListener('click', function () {
+    document.querySelectorAll('.main-initial__tab')[0].classList.toggle('active');
+    document.querySelectorAll('.main-initial__tab')[1].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('active');
+    document.querySelectorAll('.aside-list__item')[0].classList.toggle('white');
+    document.querySelectorAll('.aside-list__item')[1].classList.toggle('white');
+    document.querySelector('html').classList.add('night');
+  });
+}
 "use strict";
 
 ;
